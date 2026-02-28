26c89fa0a2b88165742941ef8f00c862b5a08e03
viterikenneth29-bit <viterikenneth29@gmail.com>
2026-02-27 23:32:34 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,24 @@
-cconst CACHE_NAME = "kenneth-briggette-v2";
+const CACHE_NAME = "kenneth-briggette-v3";
+
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(["./"]);
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
